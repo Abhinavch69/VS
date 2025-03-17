95e4ddae9936832965c7e5aaf10a2af54bf3ba1b
Abhinavch69 <abhinavchauhan2526@gmail.com>
2025-03-17 18:17:23 +0530

    permutations

---
+++ practice/src/permutation/permutation_of_string.java
@@ -0,0 +1,63 @@
+package permutation;
+import java.util.*;
+public class permutation_of_string {
+  public static void main(String[] args) {
+    permutations("", "abc");
+
+    ArrayList<String> ans=permutations2("", "abc");
+    System.out.println(ans);
+
+    System.out.println(permutationsCount("", "abc"));
+  }
+
+  static void permutations(String p, String up){
+    if(up.isEmpty()){
+      System.out.println(p);
+      return;
+    }
+
+    char ch=up.charAt(0);
+
+    for(int i=0;i<=p.length();i++){
+      String f=p.substring(0, i);
+      String s=p.substring(i,p.length());
+      permutations(f+ch+s, up.substring(1));
+    }
+  }
+
+  static ArrayList<String> permutations2(String p, String up){
+    if(up.isEmpty()){
+      ArrayList<String> list=new ArrayList<>();
+      list.add(p);
+      return list;
+    }
+
+    char ch=up.charAt(0);
+
+    //local to this call
+    ArrayList<String> ans=new ArrayList<>();
+
+    for(int i=0;i<=p.length();i++){
+      String f=p.substring(0, i);
+      String s=p.substring(i,p.length());
+      ans.addAll(permutations2(f+ch+s, up.substring(1)));
+    }
+
+    return ans;
+  }
+
+  static int permutationsCount(String p, String up){
+    if(up.isEmpty()){
+      return 1;
+    }
+    int count=0;
+    char ch=up.charAt(0);
+
+    for(int i=0;i<=p.length();i++){
+      String f=p.substring(0, i);
+      String s=p.substring(i,p.length());
+      count=count+permutationsCount(f+ch+s, up.substring(1));
+    }
+    return count;
+  }
+}
