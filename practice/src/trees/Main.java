5946f6e85312cb6c072c0c7275140e3fffd487a1
Abhinavch69 <abhinavchauhan2526@gmail.com>
2025-03-19 23:14:32 +0530

    trees

---
+++ practice/src/trees/Main.java
@@ -0,0 +1,16 @@
+package trees;
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+      //Scanner sc=new Scanner(System.in);
+      //binaryTree tree=new binaryTree();
+      //tree.populate(sc);
+      //tree.display();
+      //tree.prettyDisplay();
+
+    BST_insert tree=new BST_insert();
+    int[] nums={5,2,7,1,4,6,9,8,3,10};
+    tree.populate(nums);
+    tree.display();
+  }
+}
---
+++ practice/src/trees/binarySearchtree.java
@@ -0,0 +1,48 @@
+package trees;
+
+public class binarySearchtree {
+  class Node{
+    private int value;
+    private int height;
+    private Node left;
+    private Node right;
+
+    public Node(int value){
+      this.value = value;
+    }
+
+    public int getValue(){
+      return value;
+    }
+  }
+    private Node root;
+  
+
+    public binarySearchtree(){
+
+    }
+
+    public int height(Node node){
+      if(node==null) return -1;
+      return node.height;
+    }
+
+    public boolean isEmpty(){
+      return root==null;
+    }
+
+    public void display(){
+      display(root,"Root node: ");
+    }
+
+    private void display(Node node,String details){
+      if(node==null){
+        return;
+      }
+      System.out.println(details+node.getValue());
+
+      display(node.left, "left child of "+node.getValue()+" : ");
+      display(node.right, "right child of "+node.getValue()+" : ");
+    }
+  
+}
