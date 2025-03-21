20e3eb425eb5591bf2f9cca93a597f697f1bc5ab
Abhinavch69 <abhinavchauhan2526@gmail.com>
2025-03-21 16:27:34 +0530

    driver code for AVL tree

--- practice/src/trees/Main.java
+++ practice/src/trees/Main.java
@@ -8,9 +8,17 @@ public class Main {
       //tree.display();
       //tree.prettyDisplay();
 
-    BST_insert tree=new BST_insert();
-    int[] nums={5,2,7,1,4,6,9,8,3,10};
-    tree.populate(nums);
-    tree.display();
+    //BST_insert tree=new BST_insert();
+    //int[] nums={5,2,7,1,4,6,9,8,3,10};
+    //tree.populate(nums);
+    //tree.display();
+
+    tree2AVL tree=new tree2AVL();
+
+    for(int i=0;i<100;i++){
+      tree.insert(i);
+    }
+
+    System.out.println(tree.height());
   }
 }
