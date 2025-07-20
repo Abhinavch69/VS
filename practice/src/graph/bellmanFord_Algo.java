7e59b32edad3db218cf42c28671af398722b0efd
Abhinavch69 <abhinavchauhan2526@gmail.com>
2025-07-20 16:44:14 +0530

    shortest distance algorithm

---
+++ practice/src/graph/bellmanFord_Algo.java
@@ -0,0 +1,46 @@
+//shortest distance
+//helps to detect negative cycle
+//applicabe only on DG
+//relax all the edges N-1 times sequentially
+//Why N-1 ?
+//Sice in a graph of N nodes, in worst case, 
+//you will take N-1 edges to reach from the first to the last,
+//thereby we iterate for N-1 iterations.
+//Try drawing a graph which takes more than N-1 edges for any path,
+//it is not possible.
+
+//How to detect negative cycle
+//if at Nth iteration the distance array is still updating(means decreasing)
+//it means there is a negative cycle
+
+package graph;
+import java.util.*;
+public class bellmanFord_Algo {
+  static int[] bellman_ford(int V,List<List<Integer>> edges,int S){
+    int[] dist=new int[V];
+    Arrays.fill(dist,(int)(1e8));
+    dist[S]=0;
+    //V x E
+    for(int i=0;i<V-1;i++){
+      for(List<Integer> it:edges){
+        int u=it.get(0);
+        int v=it.get(1);
+        int wt=it.get(2);
+        if(dist[u] != 1e8 && dist[u]+wt < dist[v]){
+          dist[v]=dist[u]+wt;
+        }
+      }
+    }
+    //Nth relaxation to check negative cycle'
+    for(List<Integer> it:edges){
+      int u=it.get(0);
+      int v=it.get(1);
+      int wt=it.get(2);
+      if(dist[u] != 1e8 && dist[u]+wt < dist[v]){
+        int temp[]={-1};
+        return temp;
+      }
+    }
+    return dist;
+  }
+}
---
+++ practice/src/graph/floydWarshall_Algo.java
@@ -0,0 +1,44 @@
+//multi source shortest path algo(hr node se hr node tk smallest distace find krta h)
+//helps to detect negative cycle as well
+//using something that is pre computed
+//if the costing to travel from a node to the node itself is less than zero then we can say it's a negative cycle
+
+
+package graph;
+import java.util.*;
+public class floydWarshall_Algo {
+  public void shortest_distance(int[][] matrix){
+    int n=matrix.length;
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        if(matrix[i][j]==-1){
+          matrix[i][j]=(int)(1e9);
+        }
+        if(i==j) matrix[i][j]=0;
+      }
+    }
+    //N x N x N
+    for(int k=0;k<n;k++){
+      for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+          matrix[i][j]=Math.min(matrix[i][j],matrix[i][k]+matrix[k][j]);
+        }
+      }
+    }
+
+    //for negative cycle
+    for(int i=0;i<n;i++){
+      if(matrix[i][i]<0){
+        //print something here
+      }
+    }
+
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        if(matrix[i][j]==(int)(1e9)){
+          matrix[i][j]=-1;
+        }
+      }
+    }
+  }
+}
